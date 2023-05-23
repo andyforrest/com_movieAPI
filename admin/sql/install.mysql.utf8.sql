c3a6b4fff58ef49d93aebf67b48387eab0564865
andyforrest <46865032+andyforrest@users.noreply.github.com>
2023-05-23 14:06:28 +0100

    Update install.mysql.utf8.sql

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -10,7 +10,3 @@ CREATE TABLE `#__movieAPI` (
 	AUTO_INCREMENT =0
 	DEFAULT CHARSET=utf8mb4 
 	DEFAULT COLLATE=utf8mb4_unicode_ci;
-	
-	
--- INSERT INTO `#__movieAPI` (`url`, `key`) VALUES
--- ('api.themoviedb.org/3/discover/movie', 'b3d42014ab99493c7373eb7d36b4a2b4');
